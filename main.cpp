6693b58637cd3a56bae325b11ae07cdd66eb0d0d
Andreea Andrei <88508608+andreea0306@users.noreply.github.com>
2023-04-23 20:49:48 +0300

    Added new background for meniu

--- main.cpp
+++ main.cpp
@@ -24,6 +24,10 @@ cv::Mat getChessMap() {
 }
 
 int main() {
+
+    cv::Mat snakeImg = cv::imread("/home/andreea/CLionProjects/newSnakeTry/snake_start_menu.png");
+    cv::Mat resizedSnakeImg;
+    cv::resize(snakeImg, resizedSnakeImg,cv::Size(WIDTH, HEIGHT), INTER_LINEAR);
     // initialize the game
     Snake snake(WIDTH / 2, HEIGHT / 2);
     Food food;
@@ -31,12 +35,24 @@ int main() {
     int score = 0;
     Mat menuImg(HEIGHT, WIDTH, CV_8UC3, Scalar(0));
     menuImg = getChessMap();
-    cv::putText(menuImg,"Press Y - start game", Point(50, 50), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 2);
-    cv::putText(menuImg,"Press Q - quit the game", Point(50, 100), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 2);
-    imshow("Menu Snake",menuImg);
+
+    //cv::Mat insetImage(menuImg, cv::Rect(30, 30, 100, 100));
+    //resizedSnakeImg.copyTo(menuImg(cv::Rect(30,30,100,100)));
+
+    // Define the weight of each image in the blend
+    double alpha = 0.5;
+    double beta = 0.5;
+
+    // Create the blended image
+    Mat blended_image;
+    addWeighted(menuImg, alpha, resizedSnakeImg, beta, 0.0, blended_image);
+    cv::putText(blended_image,"START-PRESS SPACEBAR", Point(270, 100), FONT_HERSHEY_DUPLEX, 0.8, Scalar(0, 0, 0), 1);
+    cv::putText(blended_image,"QUIT- PRESS ANY KEY", Point(270, 150), FONT_HERSHEY_DUPLEX, 0.8, Scalar(0, 0, 0), 1);
+    imshow("Menu Snake",blended_image);
+
     char key = waitKey(0);
     switch (key) {
-        case 'y':
+        case ' ':
 // create the window
             destroyWindow("Menu Snake");
             namedWindow("Snake Game", WINDOW_NORMAL);
@@ -46,14 +62,14 @@ int main() {
             while (!game_over) {
                 // get user input
                 int key = waitKey(100);
-                if (key == 'w') {
+                if (key == 'w' || key == 'W') {
                     snake.set_direction(3);
                 }
-                else if (key == 'a') {
+                else if (key == 'a' || key == 'A') {
                     snake.set_direction(2);
-                } else if (key == 's') {
+                } else if (key == 's' || key == 'S') {
                     snake.set_direction(1);
-                } else if (key == 'd') {
+                } else if (key == 'd' || key == 'D') {
                     snake.set_direction(0);
                 }
 
@@ -65,11 +81,18 @@ int main() {
                     game_over = true;
                     Mat scoreImage(HEIGHT, WIDTH, CV_8UC3, Scalar(0));
                     scoreImage = getChessMap();
-                    cv::putText(scoreImage,"GAME OVER!", Point(50, 50), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 2);
+                    // Define the weight of each image in the blend
+                    double alpha = 0.5;
+                    double beta = 0.5;
+
+                    // Create the blended image
+                    Mat blended_image;
+                    addWeighted(scoreImage, alpha, resizedSnakeImg, beta, 0.0, blended_image);
+                    cv::putText(blended_image,"GAME OVER!", Point(270, 50), FONT_HERSHEY_DUPLEX, 1.0, Scalar(0, 0, 0), 2);
                     string s = to_string(score);
-                    cv::putText(scoreImage,"Score: " + s, Point(50, 100), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 2);
+                    cv::putText(blended_image,"Score: " + s, Point(270, 100), FONT_HERSHEY_DUPLEX, 1.0, Scalar(0, 0, 0), 2);
                     destroyWindow("Snake Game");
-                    imshow("Game Over", scoreImage);
+                    imshow("Game Over", blended_image);
                     waitKey(0);
                     break;
                 } else if (snake.body[0] == food.get_position()) {
@@ -89,7 +112,7 @@ int main() {
             waitKey(0);
             break;
 
-        case 'q':
+        default:
             cout<<"Game quitting"<<endl;
             break;
     }
